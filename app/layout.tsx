70b79af5eaaa10e9e479ab3880822da7d83bc0dc
vleras <vleras512@gmail.com>
2025-12-08 21:07:12 +0100

    /

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
--- components/ProductsSidebarMobile.tsx
+++ components/ProductsSidebarMobile.tsx
@@ -53,12 +53,22 @@ export default function ProductsSidebarMobile({
             }}
           >
             <Image
-              src="/avacr7.png"
-              alt="AVACR7"
-              width={200}
-              height={100}
-              className="sidebar-logo-image-mobile"
+              src="/avalogo.svg"
+              alt="AVA Logo"
+              width={100}
+              height={70}
+              className="sidebar-logo-image"
               priority
+            />
+          </div>
+          <div className="sidebar-cr7">
+            BY
+            <Image
+              src="/cr7.jpg"
+              alt="7"
+              width={20}
+              height={20}
+              className="sidebar-cr7-number"
             />
           </div>
         </div>
