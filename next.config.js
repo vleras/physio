8f41002ed4119dccff8f55d1bd5efcbd0e9b7877
vleras <vleras512@gmail.com>
2025-12-29 20:48:15 +0100

    optimization issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*.supabase.co",
+//         pathname: "/storage/v1/object/public/**",
+//       },
+//     ],
+//     formats: ['image/avif', 'image/webp'],
+//   },
+//   // Optimize for production
+//   compress: true,
+//   poweredByHeader: false,
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true, // 👈 ADD THIS LINE
+
     remotePatterns: [
       {
         protocol: "https",
